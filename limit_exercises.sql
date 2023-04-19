21c23fe7c6838022ee94e04869bf8f79f3729b97
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-04-19 15:09:23 -0400

    limit exercises complete

---
+++ limit_exercises.sql
@@ -0,0 +1,16 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
