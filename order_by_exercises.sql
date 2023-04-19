10db14c59521325384dbf33f8626fe4e280a3cb6
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-04-19 14:43:00 -0400

    order_by exercises complete

---
+++ order_by_exercises.sql
@@ -0,0 +1,28 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+ORDER BY emp_no;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
